a5ef5c67f638c6bb8515c92e304c216469ef1664
cfunk6 <cfunk@sourceallies.com>
2024-01-10 16:17:30 -0600

    add bucket policy to test access

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { PolicyStatement, ArnPrincipal } from 'aws-cdk-lib/aws-iam';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
@@ -14,10 +15,25 @@ export class CdkStack extends cdk.Stack {
       publicReadAccess: false
     });
 
+    const arnPrincipalID = `arn:aws:iam::${props.env?.account}:user/cfunk@sourceallies.com`
+
+    bucket.addToResourcePolicy(
+      new PolicyStatement({
+        resources: [
+          bucket.arnForObjects("*"),
+          bucket.bucketArn,
+        ],
+        actions: ["s3:List*", "S3:Get*"],
+        principals: [new ArnPrincipal(arnPrincipalID)]
+      })
+    )
+
     new BucketDeployment(this, 'devopsdsm-bucket-deployment', {
       sources: [Source.asset('./app')],
       destinationBucket: bucket
-    })
+    });
+
+
 
   }
 }
