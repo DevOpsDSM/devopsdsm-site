0ae4ec554f7daa80684e924db454da37124b36bc
cfunk6 <cfunk@sourceallies.com>
2024-01-11 11:49:43 -0600

    test with manual specific bucket policy

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,30 +1,40 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Bucket, BucketEncryption, ObjectOwnership } from 'aws-cdk-lib/aws-s3';
+import { Bucket, BucketEncryption, BucketPolicy, ObjectOwnership } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
-import { PolicyStatement, ArnPrincipal } from 'aws-cdk-lib/aws-iam';
+import { AccountPrincipal, PolicyStatement, } from 'aws-cdk-lib/aws-iam';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props as Record<string, unknown>);
 
-    const bucket = new Bucket(this, 'devopsdsm-bucket', {
+    const s3bucket = new Bucket(this, 'devopsdsm-bucket', {
       bucketName: 'devopsdsm-site-bucket',
       encryption: BucketEncryption.S3_MANAGED,
       websiteIndexDocument: 'index.html',
       objectOwnership: ObjectOwnership.OBJECT_WRITER,
-      publicReadAccess: true,
 	    blockPublicAccess: {
 		    blockPublicPolicy: false,
 		    blockPublicAcls: false,
 		    ignorePublicAcls: false,
 		    restrictPublicBuckets: false,
-	},
+	    },
     });
+
+    s3bucket.addToResourcePolicy(
+      new PolicyStatement({
+        resources: [
+          s3bucket.arnForObjects("*"),
+          s3bucket.bucketArn
+        ],
+        actions: ["s3:List*", "S3:Get*"],
+        principals: [new AccountPrincipal(this.account)]
+      })
+    );
 
     new BucketDeployment(this, 'devopsdsm-bucket-deployment', {
       sources: [Source.asset('./app')],
-      destinationBucket: bucket,
+      destinationBucket: s3bucket
     });
 
   }
