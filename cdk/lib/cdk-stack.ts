84aec53d931dc03e8aae92ddae4da4b3b27eee28
cfunk6 <cfunk@sourceallies.com>
2024-01-11 11:11:37 -0600

    add objectownership clause

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
+import { Bucket, BucketEncryption, ObjectOwnership } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import { PolicyStatement, ArnPrincipal } from 'aws-cdk-lib/aws-iam';
 
@@ -11,19 +11,14 @@ export class CdkStack extends cdk.Stack {
     const bucket = new Bucket(this, 'devopsdsm-bucket', {
       bucketName: 'devopsdsm-site-bucket',
       encryption: BucketEncryption.S3_MANAGED,
-      websiteIndexDocument: 'index.html'
+      websiteIndexDocument: 'index.html',
+      objectOwnership: ObjectOwnership.OBJECT_WRITER
     });
-
-    bucket.grantPublicAccess()
-
-    const arnPrincipalID = `arn:aws:iam::${props.env?.account}:user/cfunk@sourceallies.com`
 
     new BucketDeployment(this, 'devopsdsm-bucket-deployment', {
       sources: [Source.asset('./app')],
       destinationBucket: bucket,
     });
-
-
 
   }
 }
