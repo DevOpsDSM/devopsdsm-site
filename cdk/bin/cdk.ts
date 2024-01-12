3e18bdef3349f0fc392a470e45126660c82bbf5a
cfunk6 <cfunk@sourceallies.com>
2024-01-12 09:31:06 -0600

    change stack and bucket name for a new bucket

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import { BucketStack } from '../lib/bucket-stack';
 import { config } from '../config';
 
 const app = new cdk.App();
 
 const appConfig = config as cdk.StackProps;
 
-new CdkStack(app, 'CdkStack', {
+new BucketStack(app, 'BucketStack', {
   ...appConfig
 });
 
