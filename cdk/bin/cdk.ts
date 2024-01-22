0e2aaf97e7b74ff5a90ea9fbaa4a7235c9dd8f12
cfunk6 <cfunk@sourceallies.com>
2024-01-22 12:14:17 -0600

    idk try this i guess

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -2,13 +2,12 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
-import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 
 const app = new cdk.App();
 
 new CdkStack(app, 'CdkStack', {
   env: {
-    account: process.env.aws_account,
-    region: process.env.aws_account
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
   }
 }); 
